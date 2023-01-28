efe91f3a088e6789a555170ac4742d16c23d6139
Jaider Angarita <jaideraao@icloud.com>
2023-01-27 19:14:31 -0500

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,25 @@
+//
+// main.c
+// App
+//
+// Copyright (C) 2017 Jaider Angarita.
+// All rights reserved.
+//
+
+#define USE_CMSIS_INIT
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include "sam.h"
+
+int main(void)
+{
+	SystemInit();
+
+	while (true)
+	{
+		__asm("nop");
+	}
+
+	return EXIT_SUCCESS;
+}
