7a873dfb41c9722f825d1ed1971eb09613e354bc
Jaider Angarita <jaideraao@icloud.com>
2023-02-01 13:38:34 -0500

    se ha eliminado cmsis/samd21

--- sources/main.c
+++ sources/main.c
@@ -16,13 +16,35 @@
 
 int main(void)
 {
+	__disable_irq();
+
 	SystemInit();
 
+	// REG_PORT_DIR0 = PORT_PA17;
+	PORT->Group[0].DIRSET.bit.DIRSET = PORT_PA17;
+	PORT->Group[0].PINCFG[17UL].bit.DRVSTR = 1;
+
+	SysTick->CTRL = 0;
+	SysTick->LOAD = 1000000UL;
+	SysTick->VAL = 0;
+	SysTick->CTRL = 0x00000007;
+
+	NVIC_SetPriority(SysTick_IRQn, 3);
+	NVIC_EnableIRQ(SysTick_IRQn);
+
+	__enable_irq();
+
 	/* Replace with your application code */
+
 	while (true)
 	{
-		__asm("nop");
+		__NOP();
 	}
 
 	return EXIT_SUCCESS;
+}
+
+void SysTick_Handler(void)
+{
+	PORT->Group[0].OUTTGL.bit.OUTTGL = PORT_PA17;
 }
